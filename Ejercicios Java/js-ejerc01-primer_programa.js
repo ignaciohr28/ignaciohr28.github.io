2ba2d1d27210ff3e783dd50fd49b014cc041f10c
ignaciohr28 <96285627+ignaciohr28@users.noreply.github.com>
2022-03-28 10:55:39 +0200

    Update js-ejerc01-primer_programa.js

--- Ejercicios Java/js-ejerc01-primer_programa.js
+++ Ejercicios Java/js-ejerc01-primer_programa.js
@@ -1,1 +1,2 @@
-alert("Esto lo ejecuto desde un archivo externo!") 
+alert("Esto lo ejecuto desde un archivo externo!") //esto sirve para dar informacion sobre lo que te de la gana 
+                                                   //en una página web sin tener que estar siempre presente.
